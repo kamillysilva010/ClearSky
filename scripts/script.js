1355f0c1fb3a91ab43c215b4b97bb0363f21474f
Kamilly Vitoria Rodrigues Silva <kamillyr93@gmail.com>
2024-10-09 15:08:51 -0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ const nomeCidade = document.querySelector('.nomeCidade')
 const temperatura = document.querySelector('.temperatura')
 const descricao = document.querySelector('.descricao')
 const enter = document.querySelector('.search')
+const icon = document.querySelector('.icon')
 
 
 
@@ -23,7 +24,13 @@ const lerDados = async(cidade) =>{
 
     nomeCidade.innerHTML = data.name
     temperatura.innerHTML = data.main.temp
-    descricao.innerHTML = data.weather[0].description
+    // descricao.innerHTML = data.weather[0].description
+
+    const icons_url = data.weather[0].icon 
+    
+    icon.src = `https://openweathermap.org/img/wn/${icons_url}.png`
+    console.log(icon);
+    
     
 }
 
